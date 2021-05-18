31a0880ba4827eda82f4050654d8d7537e6b93d0
Rab-son <rabsonsayendajnr@gmail.com>
2021-05-18 07:58:09 -0700

    seventh code

---
+++ Seventh Code/src/components/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Header from './Header';
+import ContestList from './ContestList';
+import Contest from './Contest';
+import * as api from '../api';
+
+const pushState = (obj, url) =>
+  window.history.pushState(obj, '', url);
+
+class App extends React.Component {
+  static propTypes = {
+    initialData: React.PropTypes.object.isRequired
+  };
+  state = this.props.initialData;
+  componentDidMount() {
+
+  }
+  componentWillUnmount() {
+    // clean timers, listeners
+  }
+  fetchContest = (contestId) => {
+    pushState(
+      { currentContestId: contestId },
+      `/contest/${contestId}`
+    );
+    api.fetchContest(contestId).then(contest => {
+      this.setState({
+        currentContestId: contest.id,
+        contests: {
+          ...this.state.cotests,
+          [contest.id]: contest
+        }
+      });
+    });
+  };
+  fetchContestList = () => {
+    pushState(
+      { currentContestId: null },
+      '/'
+    );
+    api.fetchContestList().then(contests => {
+      this.setState({
+        currentContestId: null,
+        contests
+      });
+    });
+  };
+  currentContest() {
+    return this.state.contests[this.state.currentContestId];
+  }
+  pageHeader() {
+    if (this.state.currentContestId) {
+      return this.currentContest().contestName;
+    }
+
+    return 'Naming Contests';
+  }
+  currentContent() {
+    if (this.state.currentContestId) {
+      return <Contest
+               contestListClick={this.fetchContestList}
+               {...this.currentContest()} />;
+    }
+
+    return <ContestList
+            onContestClick={this.fetchContest}
+            contests={this.state.contests} />;
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.pageHeader()} />
+        {this.currentContent()}
+      </div>
+    );
+  }
+}
+
+export default App;
