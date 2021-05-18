6fe6081945ffe371c5b6245d36d1b64430fe2eb3
Rab-son <rabsonsayendajnr@gmail.com>
2021-05-18 07:57:05 -0700

    third code

---
+++ Third Code/server.js
@@ -0,0 +1,33 @@
+import config from './config';
+import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+
+import express from 'express';
+const server = express();
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
+
+server.set('view engine', 'ejs');
+
+import serverRender from './serverRender';
+
+server.get('/', (req, res) => {
+  serverRender()
+    .then(content => {
+      res.render('index', {
+        content
+      });
+    })
+    .catch(console.error);
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, config.host, () => {
+  console.info('Express listening on port', config.port);
+});
